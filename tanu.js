e9eb546394e01b778f7f6038d094c35e9a676aac
TANMOY PRAMANICK <pramanickt15@gmail.com>
2024-05-13 19:48:14 +0530

    Add files via upload

--- tanu.js
+++ tanu.js
@@ -1,5 +1,15 @@
-function toggleLight() {
-    var light = document.getElementById('light');
-    light.classList.toggle('on');
-  }
-  
+const lightGrid = document.getElementById('light-grid');
+
+const numLights = 171; // Change this number to adjust the grid size
+
+for (let i = 0; i < numLights; i++) {
+  const light = document.createElement('div');
+  light.classList.add('light');
+  light.addEventListener('click', toggleLight);
+  lightGrid.appendChild(light);
+}
+
+function toggleLight(event) {
+  const light = event.target;
+  light.classList.toggle('on');
+}
